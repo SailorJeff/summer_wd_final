6ad05320fbf426bb30de7ea8a53f3cf2d59fc701
Jeffrey Lee <jfrele14@gmail.com>
2021-07-15 15:56:09 -0400

    2d commit. 2d page working w proper integration, addToCart() still not working correctly

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+var shoppingCart = [];
+
+function addToCart(title, price) {
+    console.log("This has been clicked as well");
+    openModal()
+    var modal = document.getElementById('modal');
+    var product = {};
+    productTitle = title;
+    productPrice = price;
+    shoppingCart.push(product);
+    displayShoppingCart();
+}
+
+function displayShoppingCart() {
+    var totalPrice = 0;
+    var displayTitle = document.getElementById("displayTitle");
+    var displayPrice = document.getElementById("displayPrice");
+    for (var product in shoppingCart) {
+        displayTitle.innerHTML = shoppingCart.productTitle;
+        displayPrice.innerHTML = shoppingCart.productPrice;
+    }
+}
+
+var item1 = document.getElementById("item1");
+item1.addEventListener("click", addToCart("Awesome Sauce",'$49.29'));
+
 function openModal() {
     console.log("This has been clicked");
     var modal = document.getElementById("modal");
@@ -10,9 +36,4 @@ function closeModal() {
     var modal = document.getElementById("modal");
     
     modal.style.visibility = "hidden";
-}
-
-function addToCart() {
-    console.log("This has been clicked as well");
-    var modal = document.getElementById('modal');
 }
